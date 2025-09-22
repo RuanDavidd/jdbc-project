be3ada241e620ba2725045da7bb478227f1794dc
Ruan David <ruandaviddev@gmail.com>
2025-09-22 01:40:38 -0300

    update ConnectionFactory

--- src/ConnectionFactory.java
+++ src/ConnectionFactory.java
@@ -7,12 +7,12 @@ import java.sql.SQLException;
 public class ConnectionFactory {
 
     public DataSource dataSource;
-
     public ConnectionFactory(){
         ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
-        comboPooledDataSource.setJdbcUrl("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC");
+        comboPooledDataSource.setJdbcUrl("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC");//url de acordo com o bd utilizado. neste caso, o mysql.
         comboPooledDataSource.setUser("loja_virtual_user");
         comboPooledDataSource.setPassword("#S3nh4$D1f1c1l#");
+
 
         this.dataSource = comboPooledDataSource;
     }
